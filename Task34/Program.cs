c102dcc6e172860b1ffff373d4fb65910336fa9c
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-22 10:11:32 +0300

    Task34 done

---
+++ Task34/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 34
+void InputArray (int[] col) //Создание положительного трехзначного случайного массива
+{
+    for(int i=0; i<col.Length;i++)
+    {
+        col[i]=new Random().Next(100,1001);
+    }
+}
+
+int EvenNumbersArray (int [] col) // Количество четных чисел в массиве
+{
+    int count = 0;
+    for(int i=0; i<col.Length;i++)
+    {
+        if(col[i]%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+
+}
+
+Console.Clear();
+int [] arr =new int[10];
+InputArray(arr);
+int count=EvenNumbersArray(arr);
+Console.WriteLine($"{String.Join(",",arr)}");
+Console.WriteLine(count);
