07b9859442efef3aae1b3ab1b0f058aff45308a4
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-22 10:42:12 +0300

    Task36 done

---
+++ Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray (int[] col, int min, int max) //Создание случайного массива от мин до мах
+{
+    for(int i=0; i<col.Length;i++)
+    {
+        col[i]=new Random().Next(min,max+1);
+    }
+}
+
+int SumOddArrayElements(int [] col) //Сумма элементов, стоящих на нечётных позициях
+{
+    int sum=0;
+    for(int i=1;i<col.Length;i+=2)
+    {
+        sum=sum+col[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+int [] arr =new int[10];
+InputArray(arr,-10,10);
+Console.WriteLine($"{String.Join(",",arr)}");
+Console.WriteLine(SumOddArrayElements(arr));
